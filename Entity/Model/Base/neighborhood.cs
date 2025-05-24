91ea994f61db4a228c5cb57d1e3790bf4411c038
Daniel <Daniel3204155185@gmail.com>
2025-05-24 10:14:59 -0500

    agregar

--- Entity/Model/Base/neighborhood.cs
+++ Entity/Model/Base/neighborhood.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Entity.Model.Base
 {
-     public class Neighborhood
+    public class Neighborhood : BaseEntity
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int CityId { get; set; }
 
+        // Navigation properties
+        public virtual City? City { get; set; }
+        public virtual ICollection<Person>? People { get; set; }
+        public virtual ICollection<Client>? Clients { get; set; }
+        public virtual ICollection<Provider>? Providers { get; set; }
+        public virtual ICollection<Employee>? Employees { get; set; }
     }
 }
